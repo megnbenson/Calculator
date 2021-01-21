7f85966d351c01bbf26c0a13d535dc190b0fbee3
Megan Benson <MBenson@spartaglobal.com>
2021-01-21 09:59:02 +0000

    calculator with a few more test cases

--- CalculatorLib/CalculatorTests/UnitTest1.cs
+++ CalculatorLib/CalculatorTests/UnitTest1.cs
@@ -7,6 +7,7 @@ namespace CalculatorTests
     {
         //Add
         [TestCase(1,8,9)]
+        [TestCase(2, 2, 4)]
         public void AddTest(int num1, int num2, int expectedResult)
         {
             var result = StaticCalculator.Add(num1, num2);
@@ -14,6 +15,7 @@ namespace CalculatorTests
         }
 
         [TestCase(8, 1, 7)]
+        [TestCase(66, 2,64)]
         public void SubtractTest(int num1, int num2, int expectedResult)
         {
             var result = StaticCalculator.Subtract(num1, num2);
@@ -21,6 +23,7 @@ namespace CalculatorTests
         }
 
         [TestCase(8, 2, 4)]
+        [TestCase(10, 5, 2)]
         public void DivideTest(int num1, int num2, int expectedResult)
         {
             var result = StaticCalculator.Divide(num1, num2);
@@ -28,6 +31,7 @@ namespace CalculatorTests
         }
 
         [TestCase(2, 2, 4)]
+        [TestCase(3, 7, 21)]
         public void MultiplyTest(int num1, int num2, int expectedResult)
         {
             var result = StaticCalculator.Multiply(num1, num2);
@@ -35,6 +39,7 @@ namespace CalculatorTests
         }
 
         [TestCase(2, 2, 0)]
+        [TestCase(6, 4, 2)]
         public void ModulusTest(int num1, int num2, int expectedResult)
         {
             var result = StaticCalculator.Modulus(num1, num2);
